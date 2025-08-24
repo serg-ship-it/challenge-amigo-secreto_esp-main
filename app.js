c627035576f2c71d108ce206f3f1ca745d294c5c
serg-ship-it <sergiocanchi18@gmail.com>
2025-08-24 19:19:02 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+
+function agregarAmigo(){
+    let amigoSecreto = document.getElementById('amigo').value;
+    
+    if(amigoSecreto === "" || amigoSecreto === " "){
+            alert("Por favor. Para seguir ingrese un nombre.")
+    }else{ 
+        listaAmigos.push(amigoSecreto);
+        recorrerLista();
+        limpiarCaja()
+    }
+
+    console.log(listaAmigos)
+    
+    document.querySelector('#amigo').value = '';
+}
+
+function recorrerLista(){
+    let lista = document.getElementById('listaAmigos')
+    lista.innerHTML = ""; // 
+    for(let i = 0; i < listaAmigos.length; i++){
+        let elementoLista = document.createElement('li');// crea el elemento li en el html
+        elementoLista.textContent = listaAmigos[i]; // recorre la lista y agrega el contenido
+        lista.appendChild(elementoLista); // Agrega el li a la lista en el HTML
+    }
+}
+function sortearAmigo(){
+    let indiceAleatorio = Math.floor(Math.random()* listaAmigos.length);
+    let amigoAleatorio = listaAmigos[indiceAleatorio];
+    amigoAleatorio = document.getElementById('resultado');
+    amigoAleatorio.innerHTML = `El amigo secreto es: ${listaAmigos[indiceAleatorio]}`;
+}
+function limpiarCaja(){
+    //
+    document.querySelector('#amigo').value = '';
+}
+
+const listaAmigos = [];
+
+agregarAmigo();
